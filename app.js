c9a8107762d6c510b1e5209e98c4f68b47a752fd
Sylvie-B <unsolo@hotmail.fr>
2021-12-10 12:26:09 +0100

    eye

---
+++ app.js
@@ -0,0 +1,17 @@
+let arrayTotal = [];
+
+let data1 = [1,2,3];
+
+let data2 = [4,5,6];
+
+let data3 = [7,8,9];
+
+
+
+
+let test = [data1].concat([data2]);
+console.log(test);
+
+arrayTotal = test.concat(data2);
+
+
